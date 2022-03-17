83f7c8aa9cee8cd90619f78840d4924b1033fa8f
Felipe Loggan <loggan141@gmail.com>
2022-03-17 11:38:51 -0300

    Re-added list, queue and stack

---
+++ list-queue-stack/src/MainQueue.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainQueue {
+    public static void main(String[] args) {
+        //First IN, First OUT
+        //Deve ser implementada com uma list encadeada- LinkedList
+        //Queue é uma interface da interface colletion
+
+        Queue<String> car = new LinkedList<>();
+        car.add("Corolla First");
+        car.add("HB20");
+        car.add("Chevette");
+        car.add("Hilux");
+        car.add("Corolla Last");
+        car.add("Hilux");
+
+        for(String cars : car){
+            System.out.println(cars);
+        }
+
+        System.out.println("\nCarro no topo : "+car.peek()+"\n");
+
+        System.out.println("Carro que será retirado : "+car.poll()+"\n");
+
+        for(String cars : car){
+            System.out.println(cars);
+        }
+
+
+    }
+}
